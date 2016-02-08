e5fd495e961ba923bb04536637642dfe56168d49
livglm <maxmauro@solarisnet.it>
2016-02-08 11:57:25 +0100

    commit

--- __openerp__.py
+++ __openerp__.py
@@ -1,68 +1,68 @@
-    # -*- encoding: utf-8 -*-
-    ##############################################################################
-    #
-    #   sale_automatic_workflow for OpenERP
-    #   Copyright (C) 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
-    #   Copyright 2013 Camptocamp SA (author: Guewen Baconnier)
-    #
-    #   This program is free software: you can redistribute it and/or modify
-    #   it under the terms of the GNU Affero General Public License as
-    #   published by the Free Software Foundation, either version 3 of the
-    #   License, or (at your option) any later version.
-    #
-    #   This program is distributed in the hope that it will be useful,
-    #   but WITHOUT ANY WARRANTY; without even the implied warranty of
-    #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    #   GNU Affero General Public License for more details.
-    #
-    #   You should have received a copy of the GNU Affero General Public License
-    #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    #
-    ##############################################################################
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#   sale_automatic_workflow for OpenERP
+#   Copyright (C) 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
+#   Copyright 2013 Camptocamp SA (author: Guewen Baconnier)
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 {
-        'name': 'Fatturazione_automatica',
-        'version': '0.2',
-        'category': 'Generic Modules/Others',
-        'license': 'AGPL-3',
-        'description': """
-    Sale Automatic Workflow
-    =======================
+    'name': 'Fatturazione_automatica',
+    'version': '0.2',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'description': """
+Sale Automatic Workflow
+=======================
 
-    Create workflows with more or less automatization and apply it on sales
-    orders.
+Create workflows with more or less automatization and apply it on sales
+orders.
 
-    A workflow can:
+A workflow can:
 
-    - Apply default values:
-      * Packing Policy (partial, complete)
-      * Shipping Policy (prepaid, manual, postpaid, picking)
-      * Invoice On (ordered quantities, shipped quantities)
-      * Set the invoice's date to the sale order's date
+- Apply default values:
+  * Packing Policy (partial, complete)
+  * Shipping Policy (prepaid, manual, postpaid, picking)
+  * Invoice On (ordered quantities, shipped quantities)
+  * Set the invoice's date to the sale order's date
 
-    - Apply automatic actions:
-      * Validate the order (only if paid, always, never)
-      * Create an invoice
-      * Validate the invoice
-      * Confirm the picking
+- Apply automatic actions:
+  * Validate the order (only if paid, always, never)
+  * Create an invoice
+  * Validate the invoice
+  * Confirm the picking
 
-    This module is used by Magentoerpconnect and Prestashoperpconnect.
-    It is well suited for other E-Commerce connectors as well.
-    """,
-        'author': 'Massimiliano Mauro',
-        'website': 'http://www.tidielle.com/',
-        'depends': ['sale_payment_method','delivery',
-                    'stock',
-                    ],
-        'data': ['views/stock_view.xml',
-                 'views/sale_view.xml',
-                 'views/sale_workflow.xml',
-                 'views/sale_workflow_process_view.xml',
-                 'views/payment_method_view.xml',
-                 'views/account_invoice_view.xml',
-                 'custom_report_picking.xml',
-                 'views/res_view.xml',
-                 'security/ir.model.access.csv',
+This module is used by Magentoerpconnect and Prestashoperpconnect.
+It is well suited for other E-Commerce connectors as well.
+""",
+    'author': 'Massimiliano Mauro',
+    'website': 'http://www.tidielle.com/',
+    'depends': ['sale_payment_method','delivery',
+                'stock',
                 ],
-        'installable': True,
-    }
+    'data': ['views/stock_view.xml',
+             'views/sale_view.xml',
+             'views/sale_workflow.xml',
+             'views/sale_workflow_process_view.xml',
+             'views/payment_method_view.xml',
+             'views/account_invoice_view.xml',
+             'custom_report_picking.xml',
+             'views/res_view.xml',
+             'security/ir.model.access.csv',
+            ],
+    'installable': True,
+}
