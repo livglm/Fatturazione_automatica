5f3aaa67b54c9f1c6058d4aca7808f95ee4586a9
livglm <maxmauro@solarisnet.it>
2016-02-18 18:07:14 +0100

    commit

--- models/test_report.py
+++ models/test_report.py
@@ -1,12 +1,12 @@
 from openerp import api, models
 
 class ParticularReport(models.AbstractModel):
-    _name = 'report.Fatturazione_automatica.test_report'
+    _name = 'report.Fatturazione_automatica.test_report1'
 
     @api.multi
     def render_html(self, data=None):
         report_obj = self.env['report']
-        report = report_obj._get_report_from_name('Fatturazione_automatica.test_report')
+        report = report_obj._get_report_from_name('Fatturazione_automatica.test_report1')
         #self.env.cr.execute('SELECT * FROM account_invoice WHERE salesagent_id IN (select id from res_partner where salesagent = True) and residual <> 0  ORDER BY salesagent_id,partner_id, date_invoice,number ASC')
         #self.env.cr.execute('SELECT * FROM res_partner WHERE salesagent = True order by name ASC')
         #agenti = self.env.cr.fetchall()
@@ -18,4 +18,4 @@ class ParticularReport(models.AbstractModel):
         'doc_model': report.model,
         'docs': self ##self.env[report.model].search([('salesagent_id','in',agenti),('residual','<>',0)]),
         }
-        return report_obj.render('Fatturazione_automatica.test_report', docargs)
+        return report_obj.render('Fatturazione_automatica.test_report1', docargs)
