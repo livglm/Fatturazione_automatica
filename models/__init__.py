96413fc04ed2f3d332d537e430a6e24be0863f60
livglm <maxmauro@solarisnet.it>
2016-02-18 18:07:45 +0100

    commit

--- models/__init__.py
+++ models/__init__.py
@@ -17,4 +17,4 @@
 #                                                                               #
 #################################################################################
 
-from . import automatic_workflow_job,  sale, sale_workflow_process, stock,  account_invoice,test_report
+from . import automatic_workflow_job,  sale, sale_workflow_process, stock,  account_invoice
