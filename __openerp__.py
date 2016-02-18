ce546e3d343f79adfd68f1ec4184d8a7f1bc606d
livglm <maxmauro@solarisnet.it>
2016-02-18 15:10:58 +0100

    commit

--- __openerp__.py
+++ __openerp__.py
@@ -57,6 +57,7 @@ It is well suited for other E-Commerce connectors as well.
              'views/account_invoice_view.xml',
              'views/res_view.xml',
              'reports/test_report.xml',
+             'reports/account_report.xml',
              #'security/ir.model.access.csv',
             ],
     'installable': True,
