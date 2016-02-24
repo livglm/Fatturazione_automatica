704e397fce6088b800a127447c43a008183ebba3
livglm <maxmauro@solarisnet.it>
2016-02-24 11:38:57 +0100

    Merge branch 'master' of https://github.com/livglm/Fatturazione_automatica
    Conflicts:
    	__openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -56,7 +56,7 @@ It is well suited for other E-Commerce connectors as well.
              'views/sale_workflow_process_view.xml',
              'views/account_invoice_view.xml',
              'views/res_view.xml',
-              #'security/ir.model.access.csv',
+             'security/ir.model.access.csv',
             ],
     'installable': True,
 }
